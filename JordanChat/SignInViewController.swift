067279850d9d6bae909783ffb05afab8ae73999b
Jordan Matthews <hayleywoodard@Hayleys-MacBook-Pro.local>
2017-05-09 17:25:22 -0400

    Set up firebase database of users

--- JordanChat/SignInViewController.swift
+++ JordanChat/SignInViewController.swift
@@ -28,17 +28,20 @@ class SignInViewController: UIViewController {
         FIRAuth.auth()?.signIn(withEmail: emailTextField.text!, password: passwordTextField.text!, completion: { (user, error) in
             print("We tried to sign in")
             if error != nil {
-                print("Hey we have an error:\(error)")
+                print("Hey we have an error:\(String(describing: error))")
                 
                 FIRAuth.auth()?.createUser(withEmail: self.emailTextField.text!, password: self.passwordTextField.text!, completion: { (user, error) in
                     print("We tried to create a user")
                     
                     if error != nil {
-                        print("Hey we have an error:\(error)")
+                        print("Hey we have an error:\(String(describing: error))")
                         
                     } else {
                         
                         print("Created user successfully!")
+                        
+                        FIRDatabase.database().reference().child("users").child(user!.uid).child("email").setValue(user!.email!)
+                        
                         self.performSegue(withIdentifier: "signInSegue", sender: nil)
                         
                     }
