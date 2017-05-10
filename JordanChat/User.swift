038ec007c3c2ca0f21d812e78227120fe89d43e9
Jordan Matthews <hayleywoodard@Hayleys-MacBook-Pro.local>
2017-05-10 19:26:00 -0400

    User Select page updated, added to Firebase database

---
+++ JordanChat/User.swift
@@ -0,0 +1,17 @@
+//
+//  User.swift
+//  JordanChat
+//
+//  Created by Hayley Woodard on 5/9/17.
+//  Copyright © 2017 Jordan Matthews. All rights reserved.
+//
+
+import Foundation
+
+class User {
+    
+    var email = ""
+    var uid = ""
+    
+    
+}
